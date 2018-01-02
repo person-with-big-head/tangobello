40d125d97b264a6bf097a3712e68b33bfda135f9
何仕鹏 <heshipeng@heshipengdeMacBook-Air.local>
2018-01-02 09:55:21 +0800

    add popular

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -7,3 +7,41 @@ function showSearchForm() {
         search_form.style.display = "none";
     }
 }
+
+
+function initFooter(){
+
+  var ftBg = jQuery('#ft-tg-bg');
+  var hideTimeout;
+  var activeTimeout;
+  jQuery('#footer-tags a').each(function(i,e){
+     var lnk = jQuery(e);
+     var lnkWidth = lnk.width();
+     var lnkHalfWidth = lnkWidth * 0.5;
+
+     lnk.mouseenter(function(e){
+       ftBg.css({
+           'width': lnk.outerWidth() - 2,
+           'height' : lnk.outerHeight() - 2,
+           'top' : lnk.position().top,
+           'left' : lnk.position().left + 5 }
+       );
+       ftBg.addClass('active');
+       clearTimeout(hideTimeout);
+
+     });
+
+     lnk.mouseleave(function(e){
+
+       hideTimeout = setTimeout(function(){
+        ftBg.removeClass('active');
+       }, 300 );
+     });
+
+  });
+}
+
+$( function(){
+    initFooter();
+});
+
